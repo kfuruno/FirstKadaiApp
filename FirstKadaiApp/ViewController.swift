28472f1ffdc0f0bf0fb1a4c465c3e66e58473037
kiyohiro.furuno <kfuruno.pm.et.conduct@gmail.com>
2021-08-02 23:41:27 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 古野清紘 on 2021/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
